e830672456f9b724601727f38151eeb06dfeb673
nguyenuy1409 <nguyenuyy1409@gmail.com>
2024-12-20 18:56:02 +1100

    Thông báo commit

--- mylibs.h
+++ mylibs.h
@@ -1,33 +1,52 @@
-#pragma once
-#include <stdio.h>
-#include <conio.h>
+#pragma once //Avoid library conflicts when including files on top of each other
+#include <stdio.h> 
+#include <conio.h> //Library for real-time support 
 #include <ctime>
-#include <windows.h>
+#include <windows.h> 
 
 #define KEY_NONE -1
 
+//Get the current x-coordinate of the cursor.
 int whereX(){
     CONSOLE_SCREEN_BUFFER_INFO csbi;
     if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
         return csbi.dwCursorPosition.X;
     return -1;
 }
-
+//Get the current y-coordinate of the cursor.
 int whereY(){
     CONSOLE_SCREEN_BUFFER_INFO csbi;
     if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi))
         return csbi.dwCursorPosition.Y;
     return -1;
 }
-
+//Move the current cursor to the coordinates (x, y)
 void gotoXY(int x, int y){
     HANDLE hConsoleOutput;
     COORD Cursor_an_Pos = { (SHORT)x, (SHORT)y };
     hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleCursorPosition(hConsoleOutput, Cursor_an_Pos);
 }
-
+//Change the text color
 void SetColor(WORD color){
+/*
+0	Black
+1	Blue
+2	Green
+3	Aqua
+4	Red
+5	Purple
+6	Yellow
+7	White
+8	Gray
+9	Light Blue
+10	Light Green
+11	Light Aqua
+12	Light Red
+13	Light Purple
+14	Light Yellow
+15	Bright White
+*/
     HANDLE hConsoleOutput;
     hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
 
@@ -41,13 +60,13 @@ void SetColor(WORD color){
 
     SetConsoleTextAttribute(hConsoleOutput, wAttributes);
 }
-
+//Hide the mouse cursor
 void ShowCur(bool CursorVisibility){ //hide mouse pointer
     HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_CURSOR_INFO cursor = { 1, CursorVisibility };
     SetConsoleCursorInfo(handle, &cursor);
 }
-
+//Return the key code of the key pressed by the user
 int inputKey(){
     if (_kbhit()) {
         int key = _getch();
--- ransanmoi.cpp
+++ ransanmoi.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <cstdlib> //Library for function rand()
 #include "mylibs.h"
-
+void top_wall();
+void bot_wall();
+void right_wall();
+void left_wall();
+void wall();
+void snake_create(int x_coord[], int y_coord[]);
+void snake(int x_coord[], int y_coord[]);
+void snake_clear(int x_coord[], int y_coord[]);
+void snake_move(int x_coord[], int y_coord[], int x, int y, int &xfood, int &yfood);
+bool game_over(int x_coord[], int y_coord[], int x, int y);
+bool snake_eat_food(int xfood, int yfood, int x_coord[],  int y_coord[]);
+void food_create(int &xfood, int &yfood, int x_coord[], int y_coord[]);
+bool snake_ate_food(int xfood, int yfood, int x0, int y0);
 #define MAX 100
 
 using namespace std;
 
-int sl = 5; //snake beginning length 
+int sl = 4; //snake beginning length 
+int score = 0; //starting score
 void top_wall(){ //the top wall
     int x = 10, y = 1;
     while(x < 100){
@@ -39,6 +53,7 @@ void left_wall(){ //the left wall
     }
 }
 void wall(){ //save the 4 sides of the wall
+    SetColor(2);
     top_wall();
     bot_wall();
     right_wall();
@@ -69,23 +84,77 @@ void snake_clear(int x_coord[], int y_coord[]){ //remove old element
         cout << ' ';
     }
 }
-void snake_move(int x_coord[], int y_coord[], int x, int y) {
-    // Di chuyển con rắn
+void snake_move(int x_coord[], int y_coord[], int x, int y, int &xfood, int &yfood){ //Snake's moverment and handle if snake eat food
     for (int i = sl - 1; i > 0; i--) {
         x_coord[i] = x_coord[i - 1];
         y_coord[i] = y_coord[i - 1];
     }
-    x_coord[0] = x;
-    y_coord[0] = y;
+    if(snake_ate_food(xfood, yfood, x_coord[0], y_coord[0]) == false){ //
+        x_coord[0] = x;
+        y_coord[0] = y;
+    }
+    else{
+        sl++;
+        score +=1;
+        food_create(xfood, yfood, x_coord, y_coord);
+    }
+}
+bool game_over(int x_coord[], int y_coord[], int x, int y){ //Game over case
+    //Check if snake hits the walls
+    if(x <= 10 || x >= 100 || y <= 1 || y >= 26){
+        return true;
+    }
+    //Check if snake hits its body
+    for(int i = 1 ; i < sl ; i++){
+        if(x == x_coord[i] && y == y_coord[i]){
+            return true;
+        }
+    }
+    return false;
+}
+void food_create(int &xfood, int &yfood, int x_coord[], int y_coord[]){ //create food randomly on the map
+    //formula [a, b]: rand()%(b-a+1) + a
+    do{
+        //x food coordinate range [11, 99]
+        xfood = rand()%89 + 11;
+        //y food coordinate range [2, 25]
+        yfood = rand()%24 + 2; 
+    }
+    while(snake_eat_food(xfood, yfood, x_coord, y_coord) == true); 
+    gotoXY(xfood, yfood); 
+    cout << '@';
+}
+bool snake_eat_food(int xfood, int yfood, int x_coord[],  int y_coord[]){ //check if the snake ate the fruit
+    for(int i = 0 ; i < sl ; i++){
+        if(xfood == x_coord[i] && yfood == y_coord[i]){
+            return true; //the snake ate food
+        }
+    }
+    return false; //the snake has not eaten the fruit yet
+
+}
+bool snake_ate_food(int xfood, int yfood, int x0, int y0){ //check if snake ate food
+    if((x0 == xfood) && (y0 == yfood)){
+        return true; //the snake ate food
+    }
+    return false;
+}
+void score_display(){ //score table
+    gotoXY(0, 0);
+    cout << "Score: " << score;
 }
 int main(){
     ShowCur(false); //hide mouse pointer
     int x_coord[MAX], y_coord[MAX];
+
     wall();
     snake_create(x_coord, y_coord);
     snake(x_coord, y_coord);
     int x = 50, y = 13; //Starting point
     int check = 2;
+    srand(time(NULL)); //reset food position in the map
+    int xfood = 0, yfood = 0;
+    food_create(xfood, yfood, x_coord, y_coord); //create food in the map
     while(true){
         snake_clear(x_coord, y_coord);
         /*
@@ -124,9 +193,15 @@ int main(){
        else if(check == 3){ //turn left
         x--;
        }
-       snake_move(x_coord, y_coord, x, y);
+       if(game_over(x_coord, y_coord, x, y)){
+        gotoXY(50, 13); //return to starting position
+        cout << "Game Over!" << endl << "Your Score: " << score;
+        break;
+       }
+       snake_move(x_coord, y_coord, x, y, xfood, yfood);
        snake(x_coord, y_coord);
-       Sleep(200);
+       score_display();
+       Sleep(120);
     }
     return 0;
 }
